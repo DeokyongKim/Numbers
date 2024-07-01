fda99ec2fda4c168cc761e9e3646ba3d45de1327
DeokyongKim <42801399+DeokyongKim@users.noreply.github.com>
2024-07-01 14:13:41 +0000

    solving 21611, wrong answer. Changed strategy

--- BOJ_SAMSUNG/21611.hpp
+++ BOJ_SAMSUNG/21611.hpp
@@ -9,6 +9,9 @@ typedef struct _COORDINATE {
 
 int n, board[60][60], m;
 
+int standardBoard[60][60], tmpBoard[60][60];
+int standardX, standardY;
+
 int numbers[4];
 
 void printArray(int arr[60][60]) {
@@ -16,9 +19,9 @@ void printArray(int arr[60][60]) {
     for (int j = 1; j <= n; j++) {
       cout << arr[i][j] << '\t';
     }
-    cout << '\n';
+    cout << "\n\n";
   }
-  cout << '\n';
+  cout << "====================\n";
 }
 
 void blizard(int dir, int dist) {
@@ -94,6 +97,8 @@ int reduce() {
 
   int dir = 1;
 
+  int sum = 0;
+
   for (int len = 1; len <= n; len++) {
     for (int i = 0; i < len; i++) {
       if (erasing == 0) {
@@ -116,8 +121,11 @@ int reduce() {
 
             numbers[erasing] += cnt;
 
-            return cnt;
+            sum += cnt;
           }
+
+          if (board[x][y] == 0) return sum;
+
           erasing = board[x][y];
           initialize(erase);
           erase[x][y] = 1;
@@ -148,8 +156,11 @@ int reduce() {
 
             numbers[erasing] += cnt;
 
-            return cnt;
+            sum += cnt;
           }
+
+          if (board[x][y] == 0) return sum;
+          
           erasing = board[x][y];
           initialize(erase);
           erase[x][y] = 1;
@@ -162,19 +173,171 @@ int reduce() {
     dir *= -1;
   }
 
-  return 0;
+  return sum;
 }
 
 int getScore() {
   return numbers[1] + 2 * numbers[2] + 3 * numbers[3];
 }
 
+void pushTmpBoard(int first, int second) {
+  if (standardX < 1 || standardX > n || standardY < 1 || standardY > n)
+    return;
+
+  // cout << "PUSH=> CNT: " << first << " NUM: " << second << '\n';
+
+  COORDINATE direction[4] = {
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+  };
+
+  tmpBoard[standardX][standardY] = first;
+  
+  for (int dir = 0; dir < 4; dir++) {
+    int dx = direction[dir].x;
+    int dy = direction[dir].y;
+    int nx = standardX + dx;
+    int ny = standardY + dy;
+
+    int cur = standardBoard[standardX][standardY];
+
+    if (nx >= 1 && nx <= n && ny >= 1 && ny <= n) {
+      if (standardBoard[nx][ny] == cur + 1) {
+        standardX = nx;
+        standardY = ny;
+        break;
+      }
+    }
+  }
+
+  tmpBoard[standardX][standardY] = second;
+  
+  for (int dir = 0; dir < 4; dir++) {
+    int dx = direction[dir].x;
+    int dy = direction[dir].y;
+    int nx = standardX + dx;
+    int ny = standardY + dy;
+
+    int cur = standardBoard[standardX][standardY];
+
+    if (nx >= 1 && nx <= n && ny >= 1 && ny <= n) {
+      if (standardBoard[nx][ny] == cur + 1) {
+        standardX = nx;
+        standardY = ny;
+        break;
+      }
+    }
+  }
+}
+
 void changeBall() {
-  //하나의 그룹은 두 개의 구슬 A와 B로 변한다. 구슬 A의 번호는 그룹에 들어있는 구슬의 개수이고, B는 그룹을 이루고 있는 구슬의 번호이다. 구슬은 다시 그룹의 순서대로 1번 칸부터 차례대로 A, B의 순서로 칸에 들어간다. 다음 그림은 구슬이 변화한 후이고, 색은 구분하기 위해 위의 그림에 있는 그룹의 색을 그대로 사용했다. 만약, 구슬이 칸의 수보다 많아 칸에 들어가지 못하는 경우 그러한 구슬은 사라진다.
-  // shit..
+  initialize(tmpBoard);
+
+  standardX = (n + 1) / 2;
+  standardY = (n + 1) / 2 - 1;
+
+  int erasing = 0;
+  int cnt = 0;
+
+  int x = (n+1)/2;
+  int y = (n+1)/2 - 1;
+
+  int dir = 1;
+
+  bool end = false;
+
+  for (int len = 1; len <= n; len++) {
+    for (int i = 0; i < len; i++) {
+      if (board[x][y] == 0) {
+        end = true;
+        break;
+      }
+
+      if (erasing == 0) {
+        erasing = board[x][y];
+        cnt++;
+      } else {
+        if (board[x][y] == erasing) {
+          cnt++;
+        } else {
+          pushTmpBoard(cnt, erasing);
+
+          erasing = board[x][y];
+          cnt = 1;
+        }
+      }
+      x += dir;
+    }
+
+    if (end) break;
+
+    for (int i = 0; i <= len; i++) {
+      if (board[x][y] == 0) {
+        end = true;
+        break;
+      }
+
+      if (erasing == 0) {
+        erasing = board[x][y];
+        cnt++;
+      } else {
+        if (board[x][y] == erasing) {
+          cnt++;
+        } else {
+          pushTmpBoard(cnt, erasing);
+
+          erasing = board[x][y];
+          cnt = 1;
+        }
+      }
+      y += dir;
+    }
+
+    if (end) break;
+
+    dir *= -1;
+  }
+
+  pushTmpBoard(cnt, erasing);
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      board[i][j] = tmpBoard[i][j];
+    }
+  }
+}
+
+void setStandardBoard() {
+  int num = n * n;
+
+  int i = 1, j = 1;
+
+  int dir = 1;
+
+  for (int len = n; len >= 1; len--) {
+    for (int k = 0; k < len; k++) {
+      standardBoard[i][j] = num--;
+      j += dir;
+    }
+    j -= dir;
+    i += dir;
+
+    for (int k = 0; k < len-1; k++) {
+      standardBoard[i][j] = num--;
+      i += dir;
+    }
+    i -= dir;
+    j -= dir;
+
+    dir *= -1;
+  }
 }
 
 void solve() {
+  setStandardBoard();
+
   for (int i = 0; i < m; i++) {
     int direction, distance;
     cin >> direction >> distance;
@@ -192,8 +355,6 @@ void solve() {
     }
 
     changeBall();
-
-    printArray(board);
   }
 
   cout << getScore() << '\n';
