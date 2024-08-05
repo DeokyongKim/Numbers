06a1f262f941a897955f13ccfbb8bbf763066081
DeokyongKim <42801399+DeokyongKim@users.noreply.github.com>
2024-08-05 11:09:46 +0000

    solved r-k-d, nothing changed, just wrote my code again from bottom..

--- CODETREE_SAMSUNG/royal-knight-duel.hpp
+++ CODETREE_SAMSUNG/royal-knight-duel.hpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 typedef struct _KNIGHT {
-  int r;
-  int c;
-  int h;
-  int w;
-  int k;
+  int row;
+  int col;
+  int height;
+  int width;
+  int health;
 } KNIGHT;
 
 typedef struct _COORDINATE {
@@ -15,187 +16,159 @@ typedef struct _COORDINATE {
   int y;
 } COORDINATE;
 
-int wallLen, knightNum, orderNum, board[50][50], knightBoard[50][50];
-
-int totalDamage[40] = {0, };
+int L, N, Q, board[50][50], knightBoard[50][50];
 
 KNIGHT knights[40];
 
-int damageTotal = 0;
+int totalDamage[40];
 
-int knightMoved[50] = {0, };
-
-int newKnightBoard[50][50] = {0, };
-
-bool moveKnight(int id, int dirIdx) {
+void move(int id, int dir) {
   COORDINATE direction[4] = {
     {-1, 0},
     {0, 1},
     {1, 0},
-    {0, -1}
+    {0, -1},
   };
 
-  knightMoved[id] = 1;
+  int visited[40] = {0, };
 
-  int x = knights[id].r;
-  int y = knights[id].c;
+  int newKnightBoard[50][50] = {0, };
 
-  for (int i = 0; i < knights[id].h; i++) {
-    for (int j = 0; j < knights[id].w; j++) {
-      int nx = x + i + direction[dirIdx].x;
-      int ny = y + j + direction[dirIdx].y;
+  queue<int> q;
 
-      if (nx <= 0 || nx > wallLen || ny <= 0 || ny > wallLen || board[nx][ny] == 2) {
-        return false;
-      }
+  q.push(id);
+  visited[id] = 1;
 
-      newKnightBoard[nx][ny] = id;
+  int dx = direction[dir].x;
+  int dy = direction[dir].y;
 
-      if (knightBoard[nx][ny] > 0 && knightBoard[nx][ny] != id) {
-        if(!moveKnight(knightBoard[nx][ny], dirIdx)) return false;
+  while(!q.empty()) {
+    int curId = q.front();
+    q.pop();
+
+    for (int i = 0; i < knights[curId].height; i++) {
+      for (int j = 0; j < knights[curId].width; j++) {
+        int x = knights[curId].row + i;
+        int y = knights[curId].col + j;
+
+        int nx = x + dx;
+        int ny = y + dy;
+
+        if (nx <= 0 || ny <= 0 || nx > L || ny > L || board[nx][ny] == 2) {
+          return;
+        }
+
+        if (knightBoard[nx][ny] > 0 && !visited[knightBoard[nx][ny]]) {
+          visited[knightBoard[nx][ny]] = 1;
+          q.push(knightBoard[nx][ny]);
+        }
+
+        newKnightBoard[nx][ny] = curId;
       }
     }
   }
 
-  knights[id].r += direction[dirIdx].x;
-  knights[id].c += direction[dirIdx].y;
-
-  return true;
-}
-
-void clearNewKnightBoard() {
-  for (int i = 0; i <= wallLen; i++) {
-    for (int j = 0; j <= wallLen; j++) {
-      newKnightBoard[i][j] = 0;
+  for (int i = 1; i <= N; i++) {
+    if (visited[i]) {
+      knights[i].row += dx;
+      knights[i].col += dy;
     }
   }
 
-  for (int i = 1; i <= knightNum; i++) {
-    knightMoved[i] = 0;
+  
+  for (int i = 1; i <= L; i++) {
+    for (int j = 1; j <= L; j++) {
+      if (!visited[knightBoard[i][j]] && knightBoard[i][j] > 0) {
+        newKnightBoard[i][j] = knightBoard[i][j];
+      } 
+    }
   }
-}
 
-void putNewKnightBoard() {
-  for (int i = 1; i <= wallLen; i++) {
-    for (int j = 1; j <= wallLen; j++) {
+  for (int i = 1; i <= L; i++) {
+    for (int j = 1; j <= L; j++) {
       knightBoard[i][j] = newKnightBoard[i][j];
     }
   }
-}
 
-void eraseKnight(int id) {
-  for (int i = 1; i <= wallLen; i++) {
-    for (int j = 1; j <= wallLen; j++) {
-      if (knightBoard[i][j] == id) knightBoard[i][j] = 0;
+  // get damage
+  visited[id] = 0;
+
+  int eachDamage[40] = {0, };
+
+  for (int i = 1; i <= L; i++) {
+    for (int j = 1; j <= L; j++) {
+      eachDamage[knightBoard[i][j]] += (board[i][j] == 1);
     }
   }
-}
 
-void damageKnights() {
-  int damage[40] = {0, };
+  for (int i = 1; i <= N; i++) {
+    if (visited[i]) {
+      totalDamage[i] += eachDamage[i];
+    
+      knights[i].health -= eachDamage[i];
 
-  for (int i = 1; i <= wallLen; i++) {
-    for (int j = 1; j <= wallLen; j++) {
-      if (board[i][j] == 1 && knightBoard[i][j] > 0) {
-        damage[knightBoard[i][j]]++;
+      if (knights[i].health <= 0) {
+        for (int row = knights[i].row; row < knights[i].row + knights[i].height; row++) {
+          for (int col = knights[i].col; col < knights[i].col + knights[i].width; col++) {
+            knightBoard[row][col] = 0;
+          }
+        }
       }
     }
   }
 
-  for (int i = 1; i <= knightNum; i++) {
-    if (!knightMoved[i]) continue;
+  // for (int i = 1; i <= L; i++) {
+  //   for (int j = 1; j <= L; j++) {
+  //     cout << knightBoard[i][j] << ' ' ;
+  //   }
+  //   cout << '\n';
+  // }
+  // cout << '\n';
+}
 
-    knights[i].k -= damage[i];
+void order() {
+  int id, dir;
+  cin >> id >> dir;
 
-    if (knights[i].k <= 0) {
-      continue;
-      eraseKnight(i);
-    }
+  if (knights[id].health <= 0) return;
 
-    totalDamage[i] += damage[i];
-  }
+  move(id, dir);
 }
 
 int getTotalDamage() {
   int ans = 0;
-  
-  for (int i = 1; i <= knightNum; i++) {
-    // if (knights[i].k <= 0) continue;
-    ans += totalDamage[i];
+
+  for (int i = 1; i <= N; i++) {
+    if (knights[i].health > 0) ans += totalDamage[i];
   }
 
   return ans;
 }
 
-void printKnightBoard() {
-  for (int i = 1; i <= wallLen; i++) {
-    for (int j = 1; j <= wallLen; j++) {
-      cout << knightBoard[i][j] << ' ';
-    }
-    cout << '\n';
-  }
-  cout << '\n';
-}
-
-void putOldKnightBoard() {
-  for (int id = 1; id <= knightNum; id++) {
-    if (knightMoved[id] == 0) {
-      for (int i = knights[id].r; i < knights[id].r + knights[id].h; i++) {
-        for (int j = knights[id].c; j < knights[id].c + knights[id].w; j++) {
-          newKnightBoard[i][j] = id;
-        }
-      }
-    }
-  }
-}
-
-void solve() {
-  for (int order_cnt = 0; order_cnt < orderNum; order_cnt++) {
-    int knightId, direction;
-    cin >> knightId >> direction;
-
-    if (knights[knightId].k <= 0) continue;
-
-    clearNewKnightBoard();
-
-    if (moveKnight(knightId, direction)) {
-      putOldKnightBoard();
-
-      putNewKnightBoard();
-
-      knightMoved[knightId] = 0;
-
-      damageKnights();
-    }
-
-    // printKnightBoard();
-  }
-
-  cout << getTotalDamage() << '\n';
-}
-
 int run() {
-  cin >> wallLen >> knightNum >> orderNum;
+  cin >> L >> N >> Q;
 
-  for (int i = 1; i <= wallLen; i++) {
-    for (int j = 1; j <= wallLen; j++) {
+  for (int i = 1; i <= L; i++) {
+    for (int j = 1; j <= L; j++) {
       cin >> board[i][j];
     }
   }
 
-  for (int i = 1; i <= knightNum; i++) {
-    KNIGHT *tmp = &knights[i];
-
-    cin >> tmp->r >> tmp->c >> tmp->h >> tmp->w >> tmp->k; 
+  for (int id = 1; id <= N; id++) {
+    cin >> knights[id].row >> knights[id].col >> knights[id].height >> knights[id].width>> knights[id].health;
   
-    for (int row = 0; row < tmp->h; row++) {
-      for (int col = 0; col < tmp->w; col++) {
-        knightBoard[tmp->r + row][tmp->c + col] = i;
+    for (int i = 0; i < knights[id].height; i++) {
+      for (int j = 0; j < knights[id].width; j++) {
+        knightBoard[knights[id].row + i][knights[id].col + j] = id;
       }
     }
   }
 
-  solve();
+  for (int order_num = 0; order_num < Q; order_num++) {
+    order();
+  }
+
+  cout << getTotalDamage() << '\n';
 
   return 0;
 }
