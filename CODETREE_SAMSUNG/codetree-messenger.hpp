7bcc7ced35851aec21cfbc4d05b276f4b9264167
DeokyongKim <42801399+DeokyongKim@users.noreply.github.com>
2024-08-10 11:32:41 +0000

    solved codtree-messenger. ALWAYS CONSIDER POSSIBILITIES

--- CODETREE_SAMSUNG/codetree-messenger.hpp
+++ CODETREE_SAMSUNG/codetree-messenger.hpp
@@ -1,91 +1,122 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
 int n, orderNumber;
 
-int parents[100010], authority[100010];
+int parents[100010], authority[100010], alarm[100010][25];
 
-int children[100010][2];
+bool on[100010];
 
-void setParentsAndAuthority() {
+void printAlarm() {
+  for (int i = 0; i <= n; i++) {
+    cout << i << ": ";
+    for (int j = 0; j <= 20; j++) {
+      cout << alarm[i][j] << ' ' ;
+    }
+    cout << '\n';
+  }
+  cout << '\n';
+}
+
+void setParentsAndAuthority() {  
   for (int i = 1; i <= n; i++) {
     cin >> parents[i];
-    if (children[parents[i]][0] == 0) children[parents[i]][0] = i;
-    else children[parents[i]][1] = i;
+    on[i] = true;
   }
   for (int i = 1; i <= n; i++) {
     cin >> authority[i];
+    if (authority[i] > 20) authority[i] = 20;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int parent = parents[i], level = authority[i] - 1; level >= 0; parent = parents[parent], level--) {
+      alarm[parent][level]++;
+
+      if (parent == 0 || !on[parent]) break;
+    }
+  }
+}
+
+void onAndOff(int id) {
+  on[id] = !on[id];
+
+  if (!on[id]) {
+    for (int parent = parents[id], level = 1; authority[id] - level >= 0 ; parent = parents[parent], level++) {
+      alarm[parent][authority[id] - level]--;
+      if (parent == 0 || !on[parent]) break;
+    }
+    for (int parent = parents[id], level = 1; ; parent = parents[parent], level++) {
+      for (int i = level; i < 20; i++) {
+        alarm[parent][i-level] -= alarm[id][i];
+      }
+      
+      if (parent == 0 || !on[parent]) break;
+    }
+  } else {
+    for (int parent = parents[id], level = 1; authority[id] - level >= 0 ; parent = parents[parent], level++) {
+      alarm[parent][authority[id] - level]++;
+      if (parent == 0 || !on[parent]) break;
+    }
+    for (int parent = parents[id], level = 1; ; parent = parents[parent], level++) {
+      for (int i = level; i < 20; i++) {
+        alarm[parent][i-level] += alarm[id][i];
+      }
+      
+      if (parent == 0 || !on[parent]) break;
+    }
   }
 }
 
 void changeAlarmState() {
   int id;
   cin >> id;
-
-  authority[id] *= -1;
+  
+  onAndOff(id);
 }
 
 void changeAuthorityPower() {
   int id, power;
   cin >> id >> power;
 
-  if (authority[id] < 0) authority[id] = -1 * power;
-  else authority[id] = power;
+  if (power > 20) power = 20;
+
+  bool befOn = on[id];
+
+  if (on[id]) onAndOff(id);
+
+  authority[id] = power;
+
+  if (befOn) onAndOff(id);
 }
 
 void changeParent() {
   int id1, id2;
   cin >> id1 >> id2;
 
-  int parent1 = parents[id1], parent2 = parents[id2];
+  bool befOn1 = on[id1];
+  bool befOn2 = on[id2];
 
-  int c1Idx, c2Idx;
-  if (children[parent1][0] == id1) c1Idx = 0;
-  if (children[parent1][1] == id1) c1Idx = 1;
-  if (children[parent2][0] == id2) c2Idx = 0;
-  if (children[parent2][1] == id2) c2Idx = 1;
+  if (on[id1]) onAndOff(id1);
+  if (on[id2]) onAndOff(id2);
 
-  // change children array
-  int tmpChild = children[parent1][c1Idx];
-  children[parent1][c1Idx] = children[parent2][c2Idx];
-  children[parent2][c2Idx] = tmpChild;
-
-  // change parents array
-  int tmpParent = parents[id1];
+  int tmp = parents[id1];
   parents[id1] = parents[id2];
-  parents[id2] = tmpParent;
+  parents[id2] = tmp;
+
+  if (befOn1) onAndOff(id1);
+  if (befOn2) onAndOff(id2);
 }
 
 void computeNumberOfChatGetAlarm() {
-  int targetId, ans = 0;
-  cin >> targetId;
+  int id;
+  long long int ans = 0;
+  cin >> id;
 
-  typedef struct _NODE {
-    int id;
-    int level;
-  } NODE;
-
-  queue<NODE> q;
-  q.push({targetId, 0});
-
-  while(!q.empty()) {
-    int curId = q.front().id;
-    int curLv = q.front().level;
-    q.pop();
-
-    if (targetId != curId && authority[curId] <= 0) continue;
-
-    for (int i = 0; i < 2; i++) {
-      int childId = children[curId][i];
-
-      if (authority[childId] > curLv) ans++;
-
-      if (childId > 0) q.push({childId, curLv+1});
-    }
+  for (int i = 0; i < 20; i++) {
+    if (alarm[id][i] > 0) ans += alarm[id][i];
   }
-  
+
   cout << ans << '\n';
 }
 
